b9b67983e52debfd5c293177e6baa25c09a3a20c
Anderson <andersonluis1486@gmail.com>
2019-04-25 08:31:51 -0300

    Adicionado projeto Extension_Method

---
+++ Projetos_Estudo/Extension_Method/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Extension_Method.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisobj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisobj);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+            }
+
+        }
+    }
+}
---
+++ Projetos_Estudo/Extension_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Extension_Method.Extensions;
+
+
+namespace Extension_Method
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 03, 25, 11, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+                             
+        }
+    }
+}
