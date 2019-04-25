bd6d8a69eea0a18d4b58d8b9a06f04a38d88a062
Anderson <andersonluis1486@gmail.com>
2019-04-25 01:48:41 -0300

    Desenvolvida a comparação entre objetos, pelo nome

--- Projeto_IComparable/Projetos_Estudos/Program.cs
+++ Projeto_IComparable/Projetos_Estudos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Projetos_Estudos.Entities;
 
 namespace Projetos_Estudos
 {
@@ -6,7 +9,31 @@ namespace Projetos_Estudos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\Home\Documents\GitHub\00.Projetos_C#_Estudos\Projeto_IComparable\nomes.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+                catch (IOException e)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(e.Message);
+                
+                }
+            Console.ReadLine();
         }
     }
 }
