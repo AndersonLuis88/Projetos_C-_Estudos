f1b69996e4cc6649b8a63e2b50d002efec088b8a
Anderson <andersonluis1486@gmail.com>
2019-04-26 10:10:41 -0300

    Adicionado o projeto LinQ

--- Projetos_Estudo/Predicate/Program.cs
+++ Projetos_Estudo/Predicate/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Predicate.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Predicate
 {
@@ -19,16 +20,59 @@ namespace Predicate
             list.Add(new Product("Tablet", 700.00));
             list.Add(new Product("Mouse Pad", 30.00));
 
+            
+            
+            //Example Func com Lambda
+            //Func<Product, string> func = p => p.Name.ToUpper();
+
+            //Example Func referência Indireta
+            //Func<Product, string> func = NameUpper;
+            //List<string> result = list.Select(func).ToList();
+
+            //Example Func referência direta
+            List<string> result = list.Select(NameUpper).ToList();
+
+
+            //Example actions
+            //list.ForEach(UpdatePrice);
+
+            //Action<Product> act = UpdatePrice;
+            //list.ForEach(act);
+
+
+            //Actions with Lambdas
+            //Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+
+            //Example Predicate
             //Lambda InLine
             //list.RemoveAll(p => p.Price >= 100.0);
-            list.RemoveAll(ProductTest);
-            foreach (Product p in list){
-                Console.WriteLine(p);
+            //list.RemoveAll(ProductTest); //Predicate
+
+
+            //Output Data
+
+            //foreach (Product p in list){
+            //    Console.WriteLine(p);
+            //}
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
             }
         }
         public static bool ProductTest(Product p)
         {
             return p.Price >= 100.0;
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
 
     }
