a81c47af78fdcc70dc07b9763f0541cec245d7b2
Anderson <andersonluis1486@gmail.com>
2019-05-01 01:58:47 -0300

    Custom error page

--- SalesWebMVC/SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -6,6 +6,7 @@ namespace SalesWebMVC.Models.ViewModels
     public class ErrorViewModel
     {
         public string ResquestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(ResquestId);
     }
